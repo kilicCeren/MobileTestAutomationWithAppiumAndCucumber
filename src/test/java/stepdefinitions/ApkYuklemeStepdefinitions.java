214e7f67c0b8f993ceb894aac9022547827bb28f
kilicCeren <kiliccrn07@gmail.com>
2025-07-21 22:23:33 +0300

    v1

--- src/test/java/stepdefinitions/ApkYuklemeStepdefinitions.java
+++ src/test/java/stepdefinitions/ApkYuklemeStepdefinitions.java
@@ -10,7 +10,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class ApkYuklemeStepdefitinons {
+public class ApkYuklemeStepdefinitions {
 
     AndroidDriver<AndroidElement> androidDriver;
 
